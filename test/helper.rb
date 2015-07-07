b2d13fcc738779e7aa03a5df6fbdd20bad740747
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-07-07 23:29:17 +0900

    Refine test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,1 +1,30 @@
-require 'minitest/pride'
+require 'rubygems'
+require 'bundler'
+require 'pry'
+
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'fluent/test'
+unless ENV.has_key?('VERBOSE')
+  nulllogger = Object.new
+  nulllogger.instance_eval {|obj|
+    def method_missing(method, *args)
+      # pass
+    end
+  }
+  $log = nulllogger
+end
+
+require 'fluent/plugin/out_influxdb'
+
+class Test::Unit::TestCase
+end
